c25f8ec5c8f3e3ee1f537cc4b8c2984d67775b07
MuhammadAhsankhan786 <muhammadahsa079@gmail.com>
2025-07-19 01:31:58 +0500

    cleanup: Updated axios instance for production

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,25 +1,11 @@
-// src/axiosInstance.js
-// import axios from "axios";
-
-// const axiosInstance = axios.create({
-//   baseURL:
-//     window.location.href.split(":")[0] == "http"
-//       ? "http://localhost:5001/api/v1"
-//       : "/api/v1", // 🟢 ye base URL hai
-//   withCredentials: true, // 🟢 JWT Cookie bhejne k liye (Important)
-// });
-
-// export default axiosInstance;
-// src/axiosInstance.js
-
 import axios from "axios";
 
 const axiosInstance = axios.create({
   baseURL:
     window.location.hostname === "localhost"
       ? "http://localhost:5001/api/v1"
-      : "/api/v1", // 🟢 ye base URL hai,
-  withCredentials: true, // 🟢 JWT Cookie bhejne k liye (Important)
+      : "https://full-ecom-back-end.vercel.app/api/v1", // ✅ sahi production baseURL
+  withCredentials: true, // ✅ JWT Cookie bhejne k liye
 });
 
 export default axiosInstance;
